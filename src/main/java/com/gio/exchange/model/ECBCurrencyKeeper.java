8cf4663fe3c6592c918428a6969675f8f2bd34b4
GiorgosDev <mayshmaz@gmail.com>
2017-05-13 15:21:22 +0300

    load data method implementation added

--- src/main/java/com/gio/exchange/model/ECBCurrencyKeeper.java
+++ src/main/java/com/gio/exchange/model/ECBCurrencyKeeper.java
@@ -2,7 +2,12 @@ package com.gio.exchange.model;
 
 import com.gio.exchange.parsing.ECBCurrencySAXParser;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ECBCurrencyKeeper implements CurrencyKeeper {
@@ -10,12 +15,18 @@ public class ECBCurrencyKeeper implements CurrencyKeeper {
     public static final String TODAY_RATE_URL = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
     public static final String NINETY_DAYS_RATE_URL = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml";
 
-    Map<Date, Map<String,Float>> currencyRates;
+    Map<Date, Map<String,Float>> currencyRates = new HashMap<>();
     ECBCurrencySAXParser parser = new ECBCurrencySAXParser();
 
     @Override
     public void load() {
-        //todo if there's no data  - load 90 days
+        try(InputStream input = new URL(NINETY_DAYS_RATE_URL).openStream()) {
+            currencyRates = parser.parse(input);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
