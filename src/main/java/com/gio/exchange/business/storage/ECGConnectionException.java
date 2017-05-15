04c664b1cb0c10218fbfb02d51b584bf2fb2e768
GiorgosDev <mayshmaz@gmail.com>
2017-05-15 14:50:06 +0300

    exception name fixed

--- src/main/java/com/gio/exchange/business/storage/ECGConnectionException.java
+++
@@ -1,9 +0,0 @@
-package com.gio.exchange.business.storage;
-
-import com.gio.exchange.CurrencyExchangeAppException;
-
-public class ECGConnectionException extends CurrencyExchangeAppException{
-    public ECGConnectionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
