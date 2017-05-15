a18802fca3c02f1ef78cdedb3d961357e380478c
GiorgosDev <mayshmaz@gmail.com>
2017-05-15 20:05:08 +0300

    spare comments removed

--- src/main/java/com/gio/exchange/business/vo/ServiceResponse.java
+++ src/main/java/com/gio/exchange/business/vo/ServiceResponse.java
@@ -1,21 +1,11 @@
 package com.gio.exchange.business.vo;
 
 public class ServiceResponse {
-    /**
-     * Attribute to capture the Response Code to
-     * be sent to the Rest Client
-     */
+
     private int responseCode;
 
-    /**
-     * Attribute to capture the Response Message
-     * to be sent to the Rest Client
-     */
     private String message;
 
-    /**
-     * This stores field level error messages
-     */
     private Object payload;
 
     public int getResponseCode() {
